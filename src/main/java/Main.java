545da0ff1dff8f8644a06529a0b09419205dab81
Megalapka <forvsykoe@list.ru>
2022-06-10 22:51:27 +0300

    Fixed code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import ru.netology.sqr.SQRService.SQRService;
 public class Main {
     public static void main(String[] args) {
         SQRService serv = new SQRService();
-        System.out.println(serv.SearchingSQR(10,99));
+        System.out.println(serv.SearchingSQR(100, 9801));
     }
 }
--- src/main/java/ru/netology/sqr/SQRService/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService/SQRService.java
@@ -4,10 +4,12 @@ public class SQRService {
     public int SearchingSQR(int firstNumber, int lastNumber) {
 
         int countSQR = 0;
+
         for (int i = firstNumber; i <= lastNumber; i++) {
             for (int sqrNumber = 0; sqrNumber <= i; sqrNumber++) {
-                if (i == sqrNumber * sqrNumber) {
+                if ((i == sqrNumber * sqrNumber) & (sqrNumber > 9) & (sqrNumber < 100)) {
                     countSQR++;
+                    break;
                 }
             }
         }
